35aaa8ff93e809c432a780f82d7310cc134f9aa7
Chris Hodgson <hodgsonc@rogers.com>
2011-10-07 14:07:12 -0400

    first commit

---
+++ app/controllers/restaurant_controller.rb
@@ -0,0 +1,5 @@
+class RestaurantController < ApplicationController
+  def home
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fryingdutchman::Application.config.secret_token = '0c5d487281067bc74f227e86d5de83ec4c90ed5b26b189f9d9e3aecb2fc047239a8ccbb8fd14f3ef141025e702a2c946ed0372e3df8f9c9cfd7ba1e173f99db2'
