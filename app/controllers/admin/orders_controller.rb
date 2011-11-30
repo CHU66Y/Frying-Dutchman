b7670eb82ff0d4255ceb04ebe92ceddbdb3d5d79
Chris Hodgson <hodgsonc@rogers.com>
2011-11-30 15:35:02 -0500

    Fix orders, add ID to all

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,34 @@
 class Admin::OrdersController < ApplicationController
 
+     
+      def index
+        @order = Order.all  
+      end
 
+      def create
+        @order = Order.new(params[:order])
+        @order.save
+        redirect_to admin_orders_path
+      end
+
+      def new
+        @order = Order.new
+      end
+  
+      def edit
+        @order = Order.find(params[:id])
+      end
+  
+      def update
+        @order = Order.find(params[:id])
+        @order.update_attributes (params[:order])
+        redirect_to admin_orders_path
+      end
+  
+     def destroy
+      @order = Order.find(params[:id])
+        @order.destroy
+        redirect_to admin_orders_path
+    end
 
 end
