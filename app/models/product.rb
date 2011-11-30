17a367ddec940352c780b7be1088a065be98aff1
Chris Hodgson <hodgsonc@rogers.com>
2011-11-30 14:18:01 -0500

    Revert 13664210922a89d2953921ffbdde25f4ba0463d7^..HEAD

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,21 @@
 class Product < ActiveRecord::Base
   has_many :orders
   has_and_belongs_to_many :product_features
+  default_scope :order => 'title'
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  #private
+  #def ensure_not_referenced_by_any_line_item
+  #  if line_items.empty?
+  #    return true
+  #  else
+  #    errors.add(:base, 'Line Items present')
+  #    return false
+  #  end
+  # end
+
+  
   
 # validates :name, :description, :price, :presence =>true
 
