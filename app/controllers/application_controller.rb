13664210922a89d2953921ffbdde25f4ba0463d7
Chris Hodgson <hodgsonc@rogers.com>
2011-11-30 14:08:50 -0500

    Seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
    before_filter :authenticate_user!
-   
-  
-  #Private 
-  #def current_cart
-  #  Cart.find(session[:cart_id])
-  #rescue ActiveRecord::RecordNotFound
-  #  cart = Cart.create
-  #  session[:cart_id] = cart.id
-  #  cart
-  #end
+ 
   
 end
