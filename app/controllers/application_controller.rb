7066b08de7065c0bd7a1bbad59d8310c6c80d58d
Chris Hodgson <hodgsonc@rogers.com>
2011-11-30 15:57:50 -0500

    Add Cart Items Commented out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
   
    before_filter :authenticate_user!
  
+   #private
+   
+   #def current_cart Cart.find(session[:cart_id])
+   #rescue ActiveRecord::RecordNotFound cart = Cart.create session[:cart_id] = cart.id cart
+   #end
   
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,19 @@
 class Product < ActiveRecord::Base
   has_many :orders
   has_and_belongs_to_many :product_features
+#  has_many :line_items  before_destroy :ensure_not_referenced_by_any_line_item
   
 # validates :name, :description, :price, :presence =>true
+
+#private
+# ensure that there are no line items referencing this product
+#def ensure_not_referenced_by_any_line_item 
+#  if line_items.empty?
+#    return true 
+#  else
+#    errors.add(:base, 'Line Items present')
+#  return false end
+#  end
+#end
 
 end
